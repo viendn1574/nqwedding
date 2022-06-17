6149c534c2df7f1e33d9a4a97754efdd257df3c1
viendn1574 <viennd1574@gmail.com>
2022-06-17 23:49:58 +0700

    add timer
    Co-Authored-By: viendn1574 <42006461+viendn1574@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="balloon white">
+        <div className="star-red"></div>
+      <div className="face">
+        <div className="eye"></div>
+        <div className="mouth happy"></div>
+      </div>
+      <div className="triangle"></div>
+      <div className="string"></div>
+    </div>
+
+    <div className="balloon red">
+      <div className="star"></div>
+      <div className="face">
+        <div className="eye"></div>
+        <div className="mouth happy"></div>
+      </div>
+      <div className="triangle"></div>
+      <div className="string"></div>
+    </div>
+
+    <div className="balloon blue">
+      <div className="star"></div>
+      <div className="face">
+        <div className="eye"></div>
+        <div className="mouth happy"></div>
+      </div>
+      <div className="triangle"></div>
+      <div className="string"></div>
+    </div>
+      <div id="timer"></div>
+      <h1>4th of july counter</h1>
+    </div>
+  );
+}
+
+export default App;
