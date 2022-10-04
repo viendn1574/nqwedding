a05a7af05292e36d09e291adcbc95e4f898eeaf1
viendn1574 <viennd1574@gmail.com>
2022-10-05 02:19:49 +0700

    fix responsive

--- src/WeddingBody.tsx
+++ src/WeddingBody.tsx
@@ -3,18 +3,12 @@ import React from 'react';
 import Slider from "react-slick";
 import './WeddingBody.css';
 import { Swiper, SwiperSlide } from "swiper/react";
-import MapSection from './component/map/Map';
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 // import required modules
 import { Pagination, Navigation } from "swiper";
-const location = {
-    address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    lat: 37.42216,
-    lng: -122.08427,
-}
+
 function SampleNextArrow(props: any) {
     const { onClick } = props;
     return (
@@ -63,17 +57,12 @@ export class WeddingBody extends React.Component {
         prevArrow: <SamplePrevArrow />
     };
 
-    location = {
-        address: '1600 Amphitheatre Parkway, Mountain View, california.',
-        lat: 37.42216,
-        lng: -122.08427,
-    }
-
     constructor(probs: any) {    
         super(probs);
         this.imageSrc = this.imageSrc.bind(this);
         this.unSelectImg = this.unSelectImg.bind(this);
-    }
+
+    };
 
     imageSrc(e: any) {
         this.setState({currentSliderIndexUrl: e.target.getAttribute('src')});
@@ -82,18 +71,19 @@ export class WeddingBody extends React.Component {
     unSelectImg() {
         this.setState({currentSliderIndexUrl: ''});
     }
-   
+
     render(): React.ReactNode {
         const { currentSliderIndexUrl } = this.state;
+
         return (
             <>
             <Modal open={currentSliderIndexUrl !== ''} onBackdropClick={this.unSelectImg} style={{display:'flex',alignItems:'center',justifyContent:'center'}}><img style={{height: '100%'}} src={currentSliderIndexUrl} /></Modal>
             <section className="couple" id="couple">
                 <div className="container">
                     <h2 className="s-title text--colored">Happy Couple</h2>
-                    <div className="couple__groom row g-3">
-                        <div className="couple__image couple__image-groom--left border--colored col-md-6 col-lg-4 col-xl-3"></div>
-                        <div className="couple__text couple__text--right col-md-6 col-lg-4 col-xl-3">
+                    {/* <div className="couple__groom">
+                        <div className="couple__image couple__image-groom--left border--colored"></div>
+                        <div className="couple__text couple__text--right">
                             <div className="couple__text-head text--colored">
                                 <h3 className="couple__title text--colored">Jeremy</h3>
                                 <span className="couple__subtitle text--colored">the Groom</span>
@@ -103,8 +93,8 @@ export class WeddingBody extends React.Component {
                                 <p className="couple__descr">I got this amazing feeling as soon as I saw her and it lives with me every day and I promise that the bunny outfit had nothing to do with it. Needless to say it turned into a very expensive night out</p>
                             </div>
                         </div>
-                    </div>
-                    <div className="couple__bride row g-3">
+                    </div> */}
+                    {/* <div className="couple__bride row g-3">
                         <div className="couple__image couple__image-bride--right border--colored col-md-6 col-lg-4 col-xl-3"></div>
                         <div className="couple__text couple__text--left col-md-6 col-lg-4 col-xl-3">
                             <div className="couple__text-head text--colored">
@@ -115,7 +105,7 @@ export class WeddingBody extends React.Component {
                             <p className="couple__descr">More importantly, we have the same life goals, the same priorities, the same attitudes to people and money and work and the same sense of what's right and wrong in life.  We say that we're soul mates and over the last 7 years I feel like we've become adults together and somehow shaped each other into becoming better people.</p>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </section>
             <section className="story" id="story">
@@ -147,7 +137,7 @@ export class WeddingBody extends React.Component {
                 <div className="container">
                     <h2 className="s-title text--colored"> Our Photos</h2>
                     <Swiper
-                        slidesPerView={5}
+                        slidesPerView={1}
                         spaceBetween={30}
                         centeredSlides={true}
                         // pagination={{
@@ -155,18 +145,49 @@ export class WeddingBody extends React.Component {
                         //     el: '.swiper-pagination',
                         //     type: 'bullets',
                         // }}
-                        navigation={{
-                            prevEl: '.slidePrev-btn',
-                            nextEl: '.slideNext-btn'
-                        }}
-                        modules={[Navigation]}
+                        // navigation={{
+                        //     prevEl: '.slidePrev-btn',
+                        //     nextEl: '.slideNext-btn'
+                        // }}
+                        // modules={[Navigation]}
                         loop={true}
+                        breakpoints={{
+                            720: {
+                                width: 720,
+                                slidesPerView: 3,
+                              },
+
+                            920: {
+                                width: 920,
+                                slidesPerView: 3,
+                              },
+                          }}
                     >
-                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}><img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt="" onClick={this.imageSrc}/></SwiperSlide>
-                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}><img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt="" onClick={this.imageSrc}/></SwiperSlide>
-                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}><img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt="" onClick={this.imageSrc}/></SwiperSlide>
-                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}><img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt="" onClick={this.imageSrc}/></SwiperSlide>
-                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}><img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt="" onClick={this.imageSrc}/></SwiperSlide>
+                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}>
+                            <a data-fancybox data-src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" data-caption="Hello world">
+                                <img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt=""/>
+                            </a>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}>
+                            <a data-fancybox data-src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" data-caption="Hello world">
+                                <img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt=""/>
+                            </a>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}>
+                            <a data-fancybox data-src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" data-caption="Hello world">
+                                <img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt=""/>
+                            </a>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}>
+                            <a data-fancybox data-src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" data-caption="Hello world">
+                                <img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt=""/>
+                            </a>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width: '226.5px', marginRight: '32px'}}>
+                            <a data-fancybox data-src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" data-caption="Hello world">
+                                <img className="img-responsive" src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/groom.jpg" alt=""/>
+                            </a>
+                        </SwiperSlide>
                     </Swiper>
 
                 </div>
@@ -186,11 +207,12 @@ export class WeddingBody extends React.Component {
                             <time className="venues__card-data">11:30, May 16th, 2020</time>
                             </div>
                             <div className="venues__card-block"><span className="venues__card-label">Where</span><span className="venue__card-data">Saint Thomas Church Fifth Avenue 1 W 53rd St, New York, NY 10019</span></div>
-                        </div><a className="venues__card-link" href="#">See on map</a>
+                        </div>
+                        <a className="venues__card-link" href="#">See on map</a>
                         </div>
                         <div className="address__map-container--left address__map">
                             <div className="address__map--ceremony map" id="address__map--ceremony">
-                                <MapSection location={location} zoomLevel={17} />
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.838231688447!2d106.69901301474866!3d10.746947992341525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fbfac598f1d%3A0x4f93c41dae417dc0!2sMaiSon%20de%20Charme%20-%20Restaurant%20%26%20Events!5e0!3m2!1sen!2sbg!4v1664899888640!5m2!1sen!2sbg" style={{border:0 , width:'100%', height: '100%'}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
                     </div>
