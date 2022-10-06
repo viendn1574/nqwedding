0deae91459aad62fd45079401006b651ec8e9ae2
viendn1574 <viennd1574@gmail.com>
2022-10-06 23:52:16 +0700

    test use cardmedia material ui

--- src/WeddingBody.tsx
+++ src/WeddingBody.tsx
@@ -8,6 +8,7 @@ import "swiper/css/pagination";
 // import required modules
 import Gallery from './component/gallery/gallery';
 import GridGallery from './component/gallery/gridGallery';
+import { CardMedia } from '@mui/material';
 
 function SampleNextArrow(props: any) {
     const { onClick } = props;
@@ -90,10 +91,11 @@ export class WeddingBody extends React.Component {
                     <h2 className="s-title text--colored"> Câu Chuyện Tụi Mình</h2>
                     <div className="story__slide" style={{width: '100%', display: 'inline-block'}}>
                         <div className="story__slider-image">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/CHw1b_1LVBA?autoplay=1&cc_load_policy=1&mute=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; muted" allowFullScreen></iframe>
+                        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/CHw1b_1LVBA?autoplay=1&cc_load_policy=1&mute=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; muted" allowFullScreen></iframe> */}
                             {/* <video poster="PATH-TO-STILL-IMAGE" controls autoPlay width="100%" height="440" loop muted>
                                 <source src="https://nqwedding.s3.ap-southeast-1.amazonaws.com/bienhinh.MP4" type="video/mp4"/>
                             </video> */}
+                            <CardMedia width="100%" height="440" autoPlay loop muted controls component='video' src='https://nqwedding.s3.ap-southeast-1.amazonaws.com/bienhinh.MP4'/>
                         </div>
                         <div>
                             <div className="story__slider-text">
