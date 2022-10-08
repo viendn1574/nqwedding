609074eaee2ba1ef7006c87933c3d6b8034f15c2
Hoang Ta <huyhoang.ta97@gmail.com>
2022-10-08 19:00:52 +0700

    add initial photo page

--- src/component/gallery/Photo.tsx
+++ src/component/gallery/Photo.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { items } from './Photo.constants';
 
 const Photo: React.FunctionComponent = () => {
     return (
-        <>
-            Photo
-        </>
-    )
+        <Box sx={{
+            margin: "10% 5%"
+        }}> 
+            <ImageList variant="masonry" cols={3} gap={8}>
+                {items.map((item) => (
+                    <ImageListItem key={item.itemId} >
+                        <img
+                            src={`${item.mediaUrl}?fit=crop&auto=format`}
+                            alt={item.metaData.title}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </Box>
+    );
 }
 
 export default Photo;
--- src/component/gallery/gallery.tsx
+++ src/component/gallery/gallery.tsx
@@ -2,6 +2,7 @@ import { ProGallery } from 'pro-gallery';
 import 'pro-gallery/dist/statics/main.css';
 import { useState } from 'react';
 import ModalImage from '../modalImg/modalImg';
+import { items } from './Photo.constants';
 
 export default function Gallery() {
 	// Add your images here...
@@ -9,105 +10,6 @@ export default function Gallery() {
 	const [titleImg, setTitleImg] = useState('');
 	const [showFancy, setShowFancy] = useState(false);
 	
-	const items = [
-		{ // Image item:
-			itemId: '8nam1',
-			mediaUrl: 'https://nqwedding.s3.ap-southeast-1.amazonaws.com/8nam_2.JPG',
-			metaData: {
-				type: 'image',
-				height: 100,
-				width: 100,
-				title: 'sample-title',
-				description: 'sample-description',
-				focalPoint: [0, 0],
-				link: {
-					url: 'https://nqwedding.s3.ap-southeast-1.amazonaws.com/8nam_2.JPG',
-					target: '_blank'
-				},
-			}
-		},
-		{ // Another Image item:
-			itemId: '8nam2',
-			mediaUrl: 'https://nqwedding.s3.ap-southeast-1.amazonaws.com/8nam_4.JPG',
-			metaData: {
-				type: 'image',
-				height: 100,
-				width: 100,
-				title: 'sample-title',
-				description: 'sample-description',
-				focalPoint: [0, 0],
-				link: {
-					url: 'http://example.com',
-					target: '_blank'
-				},
-			}
-		},
-        { // Another Image item:
-			itemId: '8nam3',
-			mediaUrl: 'https://nqwedding.s3.ap-southeast-1.amazonaws.com/8nam_9.JPG',
-			metaData: {
-				type: 'image',
-				height: 300,
-				width: 200,
-				title: 'sample-title',
-				description: 'sample-description',
-				focalPoint: [0, 0],
-				link: {
-					url: 'http://example.com',
-					target: '_blank'
-				},
-			}
-		},
-        { // Another Image item:
-			itemId: '8nam4',
-			mediaUrl: 'https://nqwedding.s3.ap-southeast-1.amazonaws.com/8nam_11.JPG',
-			metaData: {
-				type: 'image',
-				height: 300,
-				width: 200,
-				title: 'sample-title',
-				description: 'sample-description',
-				focalPoint: [0, 0],
-				link: {
-					url: 'http://example.com',
-					target: '_blank'
-				},
-			}
-		},
-        { // Another Image item:
-			itemId: '8nam5',
-			mediaUrl: 'https://nqwedding.s3.ap-southeast-1.amazonaws.com/8nam_10.JPG',
-			metaData: {
-				type: 'image',
-				height: 100,
-				width: 200,
-				title: 'sample-title',
-				description: 'sample-description',
-				focalPoint: [0, 0],
-				link: {
-					url: 'http://example.com',
-					target: '_blank'
-				},
-			}
-		},
-        { // Another Image item:
-			itemId: '8nam6',
-			mediaUrl: 'https://nqwedding.s3.ap-southeast-1.amazonaws.com/8nam_10.JPG',
-			metaData: {
-				type: 'image',
-				height: 100,
-				width: 200,
-				title: 'sample-title',
-				description: 'sample-description',
-				focalPoint: [0, 0],
-				link: {
-					url: 'http://example.com',
-					target: '_blank'
-				},
-			}
-		},
-	]
-
 	// The options of the gallery (from the playground current state)
 	const options = {
 		galleryLayout: -1,
