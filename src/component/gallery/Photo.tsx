945ccfc749043b89ec8deb96aa4c18ff866bc01e
viendn1574 <viennd1574@gmail.com>
2022-11-19 11:29:54 +0700

    resize img

--- src/component/gallery/Photo.tsx
+++ src/component/gallery/Photo.tsx
@@ -46,12 +46,12 @@ const Photo: React.FunctionComponent = () => {
                     block: "start",
                 }
             )
-        }, 1000);
+        }, 100);
     }, [tabValue]);
 
     const renderImageList = (items: any[]) => {
         return (
-            <ImageList variant="masonry" cols={3} gap={8}>
+            <ImageList variant="woven" cols={3} gap={8}>
                 {items.map((item) => (
                     <ImageListItem key={item.img} >
                         <img
